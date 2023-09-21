29618dabc7dc942743d7b611a53882a7ef7cd639
Ezekiel Ulrich <ezekielulrich@gmail.com>
2023-09-21 15:02:16 -0400

    Polishing

--- main.py
+++ main.py
@@ -1,85 +1,46 @@
-'''
-ChatGPT generated this, probably full of errors :(
-Debug time
-'''
+from scholarly import scholarly
+import pandas as pd
 
-import requests
+authors = ['Stephen C. Graves']
+results = []
 
-api_key = '180146437ac53bfedb19d7421867592364e296a2'
+def hindex(pubs):
+    citations = [pub['num_citations'] for pub in pubs if 'num_citations' in pub]
 
-# Define your search query
-search_query = 'AU=(Associate Professor OR Professor) AND AD=Massachusetts Institute of Technology AND WC=(Manufacturing Engineering)'
+    h_index = 0
+    for i, citation_count in enumerate(citations, start=1):
+        if i <= citation_count:
+            h_index = i
+        else:
+            break
 
-api_url = 'https://api.clarivate.com/api/wos/'
-
-headers = {
-    'X-ApiKey': api_key,
-    'Content-Type': 'application/json'
-}
-
-# Perform a search to retrieve author data
-def search_author_data(query):
-    payload = {
-        "databaseId": "WOS",
-        "usrQuery": query,
-        "queryLanguage": "en",
-        "count": 100,  # Adjust as needed
-        "firstRecord": 1,
-        "fields": [
-            {
-                "name": "Relevance",
-                "sort": "D"
-            }
-        ]
-    }
-   
-    response = requests.post(api_url + 'search/query', json=payload, headers=headers)
-    return response.json()
-
-# Get the h-index for a given author
-def get_h_index(author_id):
-    payload = {
-        "databaseId": "WOS",
-        "idType": "eid",
-        "retrieveParameters": {
-            "count": 100,
-            "firstRecord": 1,
-            "fields": [
-                {
-                    "name": "D",
-                    "sort": "D"
-                }
-            ]
-        }
-    }
-   
-    response = requests.get(api_url + f'retrieve/author/{author_id}', params=payload, headers=headers)
-    print("Response:", response)
-    author_data = response.json()
-   
-    # Extract and return the h-index
-    h_index = author_data['data']['h_index']
     return h_index
 
-# Main function to collect h-index values
-def main():
-    # Perform the search
-    search_result = search_author_data(search_query)
-   
-    # Extract author data and calculate the average h-index
-    authors = search_result['Data']['Records']
-    h_indexes = []
-   
-    for author in authors:
-        author_id = author['UID']
-        h_index = get_h_index(author_id)
-        h_indexes.append(h_index)
-   
-    # Calculate the average h-index
-    average_h_index = sum(h_indexes) / len(h_indexes)
-   
-    print(f"Total Professors: {len(h_indexes)}")
-    print(f"Average h-index: {average_h_index:.2f}")
+for author in authors:
+    query = scholarly.search_author(author)
+    # Retrieve the first result from the iterator
+    first_author_result = next(query)
 
-if __name__ == "__main__":
-    main()
+    # Retrieve all the details for the author
+    author = scholarly.fill(first_author_result)
+    print(author)
+    affiliation = "Other"
+    match(author['email_domain']):
+        case '@mit.edu': 
+            affiliation = "MIT"
+        case 'umich.edu':
+            affiliation = "University of Michigan"
+        case 'northwestern.edu':
+            affiliation = "Northwestern"
+        case 'gatech.edu':
+            affiliation = "Georgia Tech"
+        case 'stanford.edu':
+            affiliation = "Stanford"
+        case 'illinois.edu':
+            affiliation = "UIUC"
+
+    results.append({'Author': author['name'], 'Affiliation': affiliation, 'Citations' : author['citedby'], 'H-index': hindex(author['publications'])})
+
+#save as csv
+df = pd.DataFrame(results)
+df.to_csv('metrics.csv', index=False)
