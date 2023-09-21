bd643799cc1e9217d39d1f202480fc53c84fa88a
Ezekiel Ulrich <ezekielulrich@gmail.com>
2023-09-21 15:23:08 -0400

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from scholarly import scholarly
 import pandas as pd
 
-authors = ['Stephen C. Graves']
+def read_names_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            names = [line.strip() for line in file.readlines()]
+        return names
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return []
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+file_path = 'names.txt'
+authors = read_names_from_file(file_path)
 results = []
+missing = False
+missingstr = f"""One or more names returned no results. 
+A common reason is the publishing name and name provided in {file_path} differ. 
+Try searching Google Scholar manually to find publishing name."""
 
 def hindex(pubs):
     citations = [pub['num_citations'] for pub in pubs if 'num_citations' in pub]
@@ -17,13 +34,14 @@ def hindex(pubs):
     return h_index
 
 for author in authors:
-    query = scholarly.search_author(author)
-    # Retrieve the first result from the iterator
-    first_author_result = next(query)
+    try:
+        author_result = next(scholarly.search_author(author))
+    except StopIteration:
+        print(f"No results for {author}")
+        missing = True
+        pass
 
-    # Retrieve all the details for the author
-    author = scholarly.fill(first_author_result)
-    print(author)
+    author = scholarly.fill(author_result)
     affiliation = "Other"
     match(author['email_domain']):
         case '@mit.edu': 
@@ -40,6 +58,9 @@ for author in authors:
             affiliation = "UIUC"
 
     results.append({'Author': author['name'], 'Affiliation': affiliation, 'Citations' : author['citedby'], 'H-index': hindex(author['publications'])})
+
+if missing:
+    print(missingstr)
 
 #save as csv
 df = pd.DataFrame(results)
